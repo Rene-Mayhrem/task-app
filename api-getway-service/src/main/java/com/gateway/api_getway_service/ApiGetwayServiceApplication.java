6132753a3ed195d85fe9667f1f161460cdd7c012
mayhrem <renecruz.0711@gmail.com>
2025-01-12 03:21:52 -0600

    Adding project

---
+++ api-getway-service/src/main/java/com/gateway/api_getway_service/ApiGetwayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.api_getway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetwayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetwayServiceApplication.class, args);
+	}
+
+}
---
+++ api-getway-service/src/test/java/com/gateway/api_getway_service/ApiGetwayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.api_getway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetwayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
